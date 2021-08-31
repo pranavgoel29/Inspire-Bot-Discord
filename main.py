3fb4368f0570b34c124eb728b3233a0d4fbb9029
pranavgoel29 <pranavgoel29@gmail.com>
2021-08-31 19:21:09 +0530

    Filtered extra code

--- main.py
+++ main.py
@@ -26,21 +26,6 @@ def get_quote():
     quote = json_data[0]['q'] + " -" + json_data[0]['a']
     return(quote)
 
-# def update_encouragements(encouraging_messages):
-#     if "encouragements" in db.keys():
-#         encouragements = db["encouragements"]
-#         encouragements.append(encouraging_messages)
-#         db["encouragements"] = encouragements
-
-#     else:
-#         db["encouragements"] = [encouraging_messages]
-
-# def delete_encouragement(index):
-#     encouragements = db["encouragements"]
-#     if len(encouragements) > index:
-#         del encouragements[index]
-#         db["encouragements"] = encouragements
-
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
@@ -55,25 +40,5 @@ async def on_message(message):
     if msg.startswith('!inspire'):
         quote = get_quote()
         await message.channel.send(quote)
-
-    # options = starter_encouragements
-    # if "encouragements" in db.keys():
-    #     options = options + db["encouragements"]
-
-    # if msg.startswith("!new_enc"):
-    #     encouraging_messages = msg.split("!new_enc ",1)[1]
-    #     update_encouragements(encouraging_messages)
-    #     await message.channel.send("New encouragement message added.")
-
-    # if msg.startswith("!del_enc"):
-    #     encouragments = []
-    #     if "encouragements" in db.keys():
-    #         index = (msg.split("!del_enc", 1)[1])
-    #         delete_encouragement(index)
-    #         encouragments = db["encouragements"]
-    #         await message.channel.send(encouragments)
-
-    if any(word in msg for word in sad_words):
-        await message.channel.send(random.choice(starter_encouragements))
  
 client.run(os.getenv('TOKEN'))
